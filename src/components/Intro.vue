2dda0fd55325969fc4e3114a454dba11c96148b1
Liam Cullen <liamtc@icloud.com>
2022-07-02 11:05:56 +1000

    nop

--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -2,9 +2,17 @@
   <div class="intro">
     <h2>Summary</h2>
     <p>
-<p>With a background in design and a love of technology, my goal is to bring beautiful and impactful designs to life on the web. My experience covers a broad range of creative practices including graphic design, motion graphics and photography – but nothing excites me more than combining all of these skills while designing for the web.</p>
-<p>I’ve had great responsibilities in my roles, including as a Retoucher, ensuring images were perfect before national publication for large Australian brands. As a Web Designer, I created a fresh and engaging Order Confirmation page for Australia’s largest online retailer.</p>
-
+      With a background in design and a love of technology, my goal is to bring
+      beautiful and impactful designs to life on the web. My experience covers a
+      broad range of creative practices including graphic design, motion
+      graphics and photography – but nothing excites me more than combining all
+      of these skills while designing for the web.
+    </p>
+    <p>
+      I’ve had great responsibilities in my roles, including as a Retoucher,
+      ensuring images were perfect before national publication for large
+      Australian brands. As a Web Designer, I created a fresh and engaging Order
+      Confirmation page for Australia’s largest online retailer.
     </p>
   </div>
 </template>
